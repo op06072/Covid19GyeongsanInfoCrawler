da60a079e78419f38ca73c6a5092710823ca8e94
op06072 <60420877+op06072@users.noreply.github.com>
2020-03-13 21:29:48 +0900

    이동 경로를 Info에 담아서 Patients에 담음

--- Covid19InfoCrawler.py
+++ Covid19InfoCrawler.py
@@ -15,16 +15,17 @@ for i in bsObject[3::2]:
     Info["확진일자"] = day[0]+"월 "+day[1]+"일"
     Info["입원기관"] = info[11].contents[0]
     Info["접촉자 수(격리조치 중 접촉자 수)"] = info[13].contents[0]
-    Patients[int(info[1].contents[0])] = Info
+    number = int(info[1].contents[0])
     where = I[3].contents[1].contents
     for j in where[1::2]:
         j = j.contents[1].contents[0]
         if j == "경로 확인중":
             When["None"] = ["경로 확인 중"]
-            Patients["이동 경로"] = When
+            Info["이동 경로"] = When
+            Patients[number] = Info
             break
 
-        if j[0] is "※":
+        if j[0] is "※": # 여기 작업중
             when = j.split("/")
             month = int(when[0])
             when = when[1].split(" ")
